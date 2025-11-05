c0f3eabbe518126e5d7d87bfcd913f4d23da8f86
brossjonk-pixel <anastantan23@gmail.com>
2025-11-05 17:47:09 +0100

    Add modifications

--- Models/FileStorage.cs
+++ Models/FileStorage.cs
@@ -1,3 +1,34 @@
+// using Newtonsoft.Json;
+// using System;
+// using System.Collections.Generic;
+// using System.IO;
+
+// namespace BlockchainSimulator.Models
+// {
+//     public static class FileStorage
+//     {
+//         private static string filePath = "blockchain_data.json";  // Path to store blockchain data
+
+//         // Save the blockchain to a JSON file
+//         public static void SaveBlockchain(List<Block> blockchain)
+//         {
+//             var jsonData = JsonConvert.SerializeObject(blockchain, Formatting.Indented);
+//             File.WriteAllText(filePath, jsonData);  // Save blockchain data to the file
+//         }
+
+//         // Load the blockchain from the JSON file
+//         public static List<Block> LoadBlockchain()
+//         {
+//             if (!File.Exists(filePath)) return new List<Block>();  // Return empty list if the file doesn't exist
+
+//             var jsonData = File.ReadAllText(filePath);  // Read JSON data from the file
+//             return JsonConvert.DeserializeObject<List<Block>>(jsonData);  // Deserialize into a list of blocks
+//         }
+//     }
+// }
+
+
+
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
@@ -7,22 +38,25 @@ namespace BlockchainSimulator.Models
 {
     public static class FileStorage
     {
-        private static string filePath = "blockchain_data.json";  // Path to store blockchain data
+        // Use AppContext.BaseDirectory so the path works on Heroku and Windows/Linux
+        private static readonly string filePath = Path.Combine(AppContext.BaseDirectory, "blockchain_data.json");
 
-        // Save the blockchain to a JSON file
         public static void SaveBlockchain(List<Block> blockchain)
         {
             var jsonData = JsonConvert.SerializeObject(blockchain, Formatting.Indented);
-            File.WriteAllText(filePath, jsonData);  // Save blockchain data to the file
+            File.WriteAllText(filePath, jsonData);
         }
 
-        // Load the blockchain from the JSON file
         public static List<Block> LoadBlockchain()
         {
-            if (!File.Exists(filePath)) return new List<Block>();  // Return empty list if the file doesn't exist
+            if (!File.Exists(filePath))
+            {
+                // Optional: create empty chain or genesis block
+                return new List<Block>();
+            }
 
-            var jsonData = File.ReadAllText(filePath);  // Read JSON data from the file
-            return JsonConvert.DeserializeObject<List<Block>>(jsonData);  // Deserialize into a list of blocks
+            var jsonData = File.ReadAllText(filePath);
+            return JsonConvert.DeserializeObject<List<Block>>(jsonData) ?? new List<Block>();
         }
     }
 }
